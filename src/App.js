2cd0e3d9bf4f0700ff9b647d82a0b3b671544fe1
Miguel Hilser <mygue0908@gmail.com>
2023-01-16 02:34:06 -0300

    correccion

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './pages/ItemListContainer/ItemListContainer';
--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,12 +1,24 @@
-import Button from 'react-bootstrap/Button';
+
+import { useState } from 'react'
 
 const ItemCount = () => {
+  const [contador, setContador]=useState(0);
+  const suma = () =>{
+    setContador(contador+1);
+  };
+  const resta = () =>{
+    if(contador<=0){
+      return;
+    }else{
+      setContador(contador-1);
+    }
+  };
   return (
     <div>
-        <div>
-        <Button variant="primary">-</Button>
-            0
-            <button>+</button>
+        <div className='controles'>
+            <button onClick={resta}>-</button>
+            <span>{contador}</span>
+            <button onClick={suma}>+</button>
         </div>
         <div>
             <button>Agregar al carrito</button>
